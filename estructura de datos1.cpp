5a8417e8c7f5d172d5586c362c1da3074956ca4a
stephanie2102 <93931535+stephanie2102@users.noreply.github.com>
2022-06-15 20:37:52 -0500

    trabajos

---
+++ estructura de datos1.cpp
@@ -0,0 +1,26 @@
+/*
+es una estructura de datos que deriva de una secuencia de datos,y cada uno de los cualescontiene un rato
+hay un nodo comienzo llamado la cabeza a frente hay un nodo de termino la cola o atras
+
+la lista solo puede ser recorrida en secuancia atras o hacia adelante
+
+operaciones de estructura
+a�adir o insertar elemento 
+eliminar o borrar elemento
+moverme e atravez de las listas desde el primero al ultimo
+buscar elementos 
+
+lista de una clase temple que nos permite crear listas que contengan
+incluye las operaciones
+
+push meter
+
+pop sacar
+
+begin()
+end()
+
+size()
+
+empy() vacio 
+*/
